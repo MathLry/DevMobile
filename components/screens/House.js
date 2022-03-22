b2c626fcda7e44964164649e3beedfba6f3670e6
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2022-03-22 16:12:41 +0100

    Add Places

--- components/screens/House.js
+++ components/screens/House.js
@@ -1,17 +1,33 @@
 import { View, ScrollView, Text, StyleSheet, FlatList} from 'react-native';
 import { useState  } from 'react';
 import HouseItem from '../houseItem';
+import {Button,Input} from 'native-base';
 
 export default function App() {
+    const [currentPlace, setCurrentPlace] = useState("");
     const [house, setHouse] = useState([
-        {text : "Salon", key:'1'},
-        {text : "Cuisine", key:'2'},
-        {text : "Salle à manger", key:'3'},
+        {text : "Salon", key:1},
+        {text : "Cuisine", key:2},
+        {text : "Salle à manger", key:3},
     ]);
-
+    
+    const addPlace = () => {
+        
+        setHouse([...house,{text : currentPlace, key: house.length+1}])
+        setCurrentPlace("");
+       
+    }
+    const addButton = <Button onPress={addPlace}>+</Button>;
     return (
         <ScrollView>
             <View style={styles.container}>
+            <Input 
+           placeholder="Ajouter une piece"
+            value={currentPlace} 
+            onChangeText={(text) => setCurrentPlace(text)}
+            rightElement={addButton}
+            >
+            </Input>
                 <View style={styles.content}>
                     <View style={styles.list}>
                         <FlatList
